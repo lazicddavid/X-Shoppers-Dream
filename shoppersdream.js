5c9ea06c7f1eaf7a7969eb386100b45342dc497f
David Lazic <lazic.ddavid@gmail.com>
2025-12-18 14:04:14 +0100

    Added .products-layout {   display: flex;   align-items: flex-start;   gap: 40px; }

--- shoppersdream.js
+++ shoppersdream.js
@@ -3,18 +3,13 @@ import { products } from "./product.js";
 const productsGrid = document.getElementById("productsGrid");
 const productsSection = document.getElementById("productsSection");
 const productsLink = document.getElementById("productsLink");
-const filterAside = document.getElementById("filterAside");
+const filtersAside = document.getElementById("filtersAside");
+
 const categoryList = document.getElementById("categoryList");
-function showProducts() {
+
+function showProducts(productsArray) {
   productsGrid.innerHTML = "";
 
-
-
-
-   function showProducts(productsArray) {
-  productsGrid.innerHTML = "";
-
-  
   products.forEach((product) => {
     const card = document.createElement("div");
     card.className = "product-card";
@@ -34,7 +29,26 @@ function showProducts() {
 productsLink.addEventListener("click", (e) => {
   e.preventDefault();
   productsSection.classList.remove("hidden");
+  filtersAside.classList.remove("hidden");
+
   showProducts();
+});
+
+categoryList.addEventListener("click", (e) => {
+  if (e.target.tagName !== "LI") return;
+
+  const category = e.target.dataset.category;
+
+  if (category === "all") {
+    showProducts(products);
+    return;
+  }
+
+  const filteredProducts = products.filter(
+    (product) => product.category === category
+  );
+
+  showProducts(filteredProducts);
 });
 
 //how to get unique values from an array

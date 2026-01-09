48ebf9c148bd1279658200970d8d9a1145518a73
David Lazic <lazic.ddavid@gmail.com>
2026-01-09 21:27:36 +0100

    Added categoryList

---
+++ shoppster.js
@@ -0,0 +1,9 @@
+import { products } from "./product";
+
+const DOM = {
+  productGrid: document.getElementById("productGrid"),
+  productsSection: document.getElementById("productsSection"),
+  productsLink: document.getElementById("productsLink"),
+  filtersAside: document.getElementById("filtersAside"),
+  categoryList: document.getElementById("categoryList"),
+};
